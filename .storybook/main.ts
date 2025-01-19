98b984dd7c200d84521f77a172d79925bbc36496
Jaime Cruz <jaimecruz.dev@gmail.com>
2025-01-18 22:20:08 -0500

    docs: ebable tables rendering in *.mdx files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,28 @@
 import type { StorybookConfig } from "@storybook/angular";
+import remarkGfm from "remark-gfm";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../components/src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../components/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../components/src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/angular",
