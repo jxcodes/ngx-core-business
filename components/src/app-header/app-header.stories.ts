7ff72d24f2cacf94eff9cae4137bb8d2c6c2440f
Jaime Cruz <jaimecruz.dev@gmail.com>
2025-03-31 01:34:35 -0500

    feat(ngbiz-app-header): add `beforeAppName`,  `afterAppName`, `content`, `beforeUser`, `afterUser` slots for adding content on specific areas

--- components/src/app-header/app-header.stories.ts
+++ components/src/app-header/app-header.stories.ts
@@ -3,6 +3,20 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { AppHeaderComponent } from './app-header.component';
 import { NgBizAppHeaderModule } from './app-header.module';
 
+const defaultArgs = {
+  appName: 'MY AWESOME APP',
+  user: {
+    email: 'jhon.doe@example.com',
+    name: 'Jhon Doe',
+  },
+  menuButtonClick: () => {
+    alert('Menu button clicked');
+  },
+  logoutButtonClick: () => {
+    alert('Logout button clicked');
+  },
+};
+
 // More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
 const meta: Meta<AppHeaderComponent> = {
   title: 'Components/AppHeader',
@@ -20,17 +34,26 @@ type Story = StoryObj<AppHeaderComponent>;
 
 // More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
 export const Default: Story = {
-  args: {
-    appName: 'MY AWESOME APP',
-    user: {
-      email: 'jhon.doe@example.com',
-      name: 'Jhon Doe',
-    },
-    menuButtonClick: () => {
-      alert('Menu button clicked');
-    },
-    logoutButtonClick: () => {
-      alert('Logout button clicked');
-    }
-  },
+  args: defaultArgs,
+};
+
+export const WithCustomSlots: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <ngbiz-app-header
+        [appName]="appName"
+        [user]="user"
+        (menuButtonClick)="menuButtonClick()"
+        (logoutButtonClick)="logoutButtonClick()"
+      >
+        <div beforeAppName style="border: 1px dashed white;">-- beforeAppName --</div>
+        <div afterAppName style="border: 1px dashed white;">-- afterAppName --</div>
+        <div content style="border: 1px dashed white; width: 100%; text-align:center;">-- content --</div>
+        <div beforeUser style="border: 1px dashed white;">-- beforeUser --</div>
+        <div afterUser style="border: 1px dashed white;">-- afterUser --</div>
+      </ngbiz-app-header>
+    `,
+  }),
+  args: defaultArgs,
 };
